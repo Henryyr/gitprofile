4c8daa1b7abc02c3e0b0757b00b8bc9d833d0fa8
henryyr <henry.360420@gmail.com>
2025-11-06 11:15:38 +0800

    fix: removing brew temporarily

--- cmd/root.go
+++ cmd/root.go
@@ -17,9 +17,16 @@ var rootCmd = &cobra.Command{
 	},
 }
 
+var version = "v0.1.0"
+
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
+
+func init() {
+	rootCmd.Version = version
+}
+
